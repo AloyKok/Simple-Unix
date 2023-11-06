aaf170cb7762169b114e23887faa8c3ca1d49238
Aloysious Kok <aloysiousksk@gmail.com>
2023-11-06 14:33:43 +0800

    Reconfigurable shell prompt (default %)

--- parser.c
+++ parser.c
@@ -6,6 +6,7 @@
  */
 
 #include "parser.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- shell.c
+++ shell.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "parser.h" 
+#include "parser.h"
 
 #define MAX_PROMPT_LEN 100
+
+// Default shell prompt (%)
 char shell_prompt[MAX_PROMPT_LEN] = "% ";
+
+const char *shell_cmds[] = {"prompt, pwd"};
 
 // Function Prototypes
 void run_shell();
-command** process_cmd_line(char *cmd_line, int new);
+command **process_cmd_line(char *cmd_line, int new);
+void builtin_prompt(command *cmd);
 
-int main() {
+int main()
+{
     run_shell(); // Start the shell
     return 0;
 }
 
-void run_shell() {
+void run_shell()
+{
     char *line = NULL;
     size_t len = 0;
     ssize_t nread;
@@ -23,9 +30,11 @@ void run_shell() {
 
     printf("%s", shell_prompt); // Display the default prompt
 
-    while ((nread = getline(&line, &len, stdin)) != -1) {
+    while ((nread = getline(&line, &len, stdin)) != -1)
+    {
         // Remove newline character from line if present
-        if(line[nread - 1] == '\n') {
+        if (line[nread - 1] == '\n')
+        {
             line[nread - 1] = '\0';
         }
 
@@ -33,26 +42,58 @@ void run_shell() {
         cmd_line = process_cmd_line(line, 1);
 
         int i;
+
         // Execute the command line
-        // This is where you would add the execution logic using the parsed command line
-        // For now, it will just print the parsed structure for demonstration purposes
-        if (cmd_line) {
+        if (cmd_line)
+        {
             int i = 0;
-            while (cmd_line[i] != NULL) {
+            while (cmd_line[i] != NULL)
+            {
                 dump_structure(cmd_line[i], i); // This is a function from your parser.c that prints the command structure
                 i++;
             }
         }
 
+        // Execute the command line
+        if (cmd_line) {
+            for (int i = 0; cmd_line[i] != NULL; i++) {
+                // Check if the command is 'prompt'
+                if (strcmp(cmd_line[i]->com_name, "prompt") == 0) {
+                    builtin_prompt(cmd_line[i]); // Change the prompt
+                } else {
+                    // Handle other commands...
+                }
+                // If this is not the last command, you might want to skip execution
+                // as the prompt command doesn't need to be 'run' like others
+            }
+        }
+
         // Free the memory allocated by getline() and the command structures
-        if (line) {
+        if (line)
+        {
             free(line);
             line = NULL;
         }
         clean_up(cmd_line, i); // Assuming clean_up() frees the entire command line structure array
 
-        printf("%s", shell_prompt); // Display the default prompt
+        printf("%s", shell_prompt);
     }
 
     free(line); // Free the memory allocated by getline()
+}
+
+// This function changes the shell prompt to the second token of the command.
+void builtin_prompt(command *cmd)
+{
+    if (cmd->argv[1] != NULL)
+    { // Check if there is a second argument
+        strncpy(shell_prompt, cmd->argv[1], MAX_PROMPT_LEN - 2);
+        size_t len = strlen(shell_prompt);
+        shell_prompt[len] = ' ';      // Add a space after the prompt
+        shell_prompt[len + 1] = '\0'; // Null-terminate the string
+    }
+    else
+    {
+        printf("prompt: expects an argument\n");
+    }
 }
